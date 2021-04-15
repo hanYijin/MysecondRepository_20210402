1d600a42c5265294b629abdf3b2b44ec0ab178fa
hanYijin <>
2021-04-16 08:55:44 +0900

    연습

---
+++ C#/Demo/Demo/Model/Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Model
+{
+    class Car
+    {
+        public const string JIJUM = "동대구영업소";
+
+        private string model;
+        private string color;
+        private string company;
+
+        public Car(string model, string color, string company)
+        {
+            this.model = model;
+            this.color = color;
+            this.company = company;
+        }
+        public string Model { get => model; set => model = value; }
+        public string Color { get => color; set => color = value; }
+        public string Company { get => company; set => company = value; }
+
+        public void printInfoCar()
+        {
+            Console.WriteLine("모델: " + model + "\n" + "색상: " + color + "\n" + "제조사: " + company + "\n");
+            Console.WriteLine("-------------------------------");
+        }
+
+        public override string ToString()
+        {
+            string str = "모델: " + model + "\n" + "제조사: " + company + "\n" + "색상: " + color + "\n" + "가격: " + price + "\n";
+            str += "------------------------";
+            return str;
+        }
+    }
+}
---
+++ C#/Demo/Demo/View/Menu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.View
+{
+    class Menu
+    {
+        public const int MENU_MAIN_RAND = 1;
+        public const int MENU_MAIN_DEL_ALL = 2;
+        public const int MENU_MAIN_VIEW = 3;
+        public const int MENU_MAIN_ADD = 4;
+        public const int MENU_MAIN_DEL = 5;
+        public const int MENU_MAIN_UPDATE = 6;
+        public const int MENU_MAIN_EXIT = 7;
+
+        public const int ADD_CAR = 1;
+        public const int ADD_CUST = 2;
+        public const int ADD_SELL = 3;
+        public const int ADD_EXIT = 4;
+
+        public const int DEL_CAR = 1;
+        public const int DEL_CUST = 2;
+        public const int DEL_SELL = 3;
+        public const int DEL_EXIT = 4;
+
+        public const int UPD_CAR = 1;
+        public const int UPD_CUST = 2;
+        public const int UPD_SELL = 3;
+        public const int UPD_EXIT = 4;
+
+        public int mainMenu()
+        {
+            int mr
+        }
+
+    }
+}
