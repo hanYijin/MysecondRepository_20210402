fa319b587630de113cc835ac27153f7d46163eba
siyoung <cookee23@naver.com>
2021-05-18 16:35:21 +0900

    ㅇㅇ

--- python/main.py
+++ python/main.py
@@ -2,8 +2,9 @@ from csvRW import doRead,doWrite
 # from myaqlrw import selectMember, insertMember
 from daegudb import read_csv_write_mysql
 from flask import Flask #flask.py Flack 클래스를 가져온다
-from covidData import covidDateSave
+#from covidData import covidDateSave
 #from csvCovid import read
+from dataset import dataSet
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,5 +18,5 @@ if __name__ == '__main__':
     # ctrl + alt + shift +l 자동정렬
     # selectMember()
     # insertMember()
-    covidDateSave()
+    dataSet()
     #read()
--- spring-work/cha/src/main/webapp/WEB-INF/views/aa.jsp
+++ spring-work/cha/src/main/webapp/WEB-INF/views/aa.jsp
@@ -3,13 +3,14 @@
 <%@ page session="false" %>
 <html>
 <head>
+	<meta charset="UTF-8">
 	<title>Home</title>
 </head>
 <body>
 <h1>
 	Hello world!  
 </h1>
-
-<P>  The time on the server is ${serverTime}. </P>
+<P>  The time on the server is ${aa}. </P>
+${bb }
 </body>
 </html>
