5c9bb537b84398970f4dc4add24dac3827636e8b
hanYijin <gksdlwls123@naver.com>
2021-06-28 15:58:14 +0900

    회원가입

---
+++ spring-work/member/src/main/java/com/org/yj/HomeController.java
@@ -0,0 +1,35 @@
+package com.org.yj;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * mybatis xml로 sql 작성
+ * sql logger select, insert, update, delete
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	
+	@Autowired
+	SqlSessionTemplate sst;
+	
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Model model) {
+		sst.insert("member.insert");
+		return "home";
+	}
+	
+}
